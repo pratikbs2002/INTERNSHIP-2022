a25c9d0b8bb425f6798023334169a5cd3744521b
Dhruvin Tandel <dhruvintandel0000@gmail.com>
2022-06-20 16:41:41 +0530

    User Profile, Formatting

--- RECORDBOOK/app/src/main/java/com/example/recordbook/currentUserProfile.java
+++ RECORDBOOK/app/src/main/java/com/example/recordbook/currentUserProfile.java
@@ -1,8 +1,8 @@
 package com.example.recordbook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class currentUserProfile extends AppCompatActivity {
 
