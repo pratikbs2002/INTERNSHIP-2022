35d433fe49ccefaf4872c261f21f07fab349fe4e
pratikbs2002 <20ce141@charusat.edu.in>
2022-06-20 11:07:33 +0530

    current user profile page

---
+++ RECORDBOOK/app/src/main/java/com/example/recordbook/currentUserProfile.java
@@ -0,0 +1,14 @@
+package com.example.recordbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class currentUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_user_profile);
+    }
+}
